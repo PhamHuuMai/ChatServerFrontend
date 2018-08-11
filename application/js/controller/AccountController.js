d12febfc5101e6d4094cac349cebd8841c973361
DESKTOP-FJ00PFP\MaiPH <mai.pham.intern@ntq-solution.com.vn>
2018-08-11 17:05:24 +0700

    Login

--- application/js/controller/AccountController.js
+++ application/js/controller/AccountController.js
@@ -1,6 +1,11 @@
-app.controller('accountCtl', ['$scope','$http','communicate','md5', function ($scope,$http,communicate,md5) {
+app.controller('accountCtl', ['$scope', 'communicate', 'md5', function ($scope, communicate, md5) {
+    $scope.alert = false;
     $scope.tabflag = false;
+    if(window.sessionStorage.getItem('token') != null){
+        window.location = '/#!/chat';
+    }
     $scope.tab = function (tab) {
+        $scope.alert = false;
         if (tab == 1) {
             $scope.tabflag = false;
         } else if (tab == 2) {
@@ -9,19 +14,59 @@ app.controller('accountCtl', ['$scope','$http','communicate','md5', function ($s
     };
 
     $scope.login = function () {
+        $scope.alert = true;
         communicate.post(
             "/login",
             {
-                email : $scope.email,
-                passwordMd5 : md5.createHash($scope.password)
+                email: $scope.email,
+                passwordMd5: md5.createHash($scope.password)
             },
-            function(responseData){
+            function (responseData) {
+                alertMsg('success', 'OK');
                 console.log(responseData);
-            },function(errorCode){
+                window.sessionStorage.setItem('token',responseData.token);
+                window.sessionStorage.setItem('user',responseData.email);
+                window.location = '/#!/chat';
+            }, function (errorCode) {
+                alertMsg('danger', 'Incorrect email or password');
                 console.log(errorCode);
             });
     };
-    $scope.register = function () {
 
+    $scope.register = function () {
+        $scope.alert = true;
+        communicate.post(
+            "/register",
+            {
+                email: $scope.emailReg,
+                password: $scope.passwordReg
+            },
+            function (responseData) {
+                alertMsg('success', 'OK');
+                console.log(responseData);
+                window.sessionStorage.setItem('token',responseData.token);
+                window.sessionStorage.setItem('user',responseData.email);
+                window.location = '/#!/chat';
+            }, function (errorCode) {
+                if (errorCode == -1)
+                    alertMsg('danger', 'Incorrect email or password.');
+                if (errorCode == 1)
+                    alertMsg('danger', 'Server error.');
+                if (errorCode == 3)
+                    alertMsg('danger', 'Email exist.');
+                console.log(errorCode);
+            });
+    };
+
+    $scope.confirm = function () {
+        $scope.alert = false;
+        if($scope.passwordReg != $scope.passwordCfr){
+            alertMsg('danger', 'Repeat password');
+            $scope.alert = true;
+        }
+    };
+    var alertMsg = function (type, msg) { 
+        $scope.type = type;
+        $scope.msg = msg;
     };
 }]);
--- application/js/service/httpService.js
+++ application/js/service/httpService.js
@@ -1,27 +1,25 @@
-app.factory('communicate', ['$http','$window', function ($http,$window) {
+app.factory('communicate', ['$http', '$window', function ($http, $window) {
     // https://chatserver-maiph.herokuapp.com/
     var service = {};
     $window.sessionStorage = {};
     service.post = function (url, request, successCallback, errorCallback) {
+        console.log($http.defaults.headers.common);
+        $http.defaults.headers.common.Authorization = $window.sessionStorage.token;
+        console.log($http.defaults.headers.common);
         $http.post(
-            apiHost + url,
-            {
-               data : request,
-               headers : {
-                    Authorization : $window.sessionStorage.token
-               }
-            }
+            method = (apiHost + url),
+            data = request
         ).then(function (response) {
-                console.log(response);
-                var data = response.data;
-                if (data.code == 0)
-                    successCallback(data.data);
-                else
-                    errorCallback(data.code);   
-            }, function (response) {
-                console.log(response);
-                errorCallback(-1);    
-            });
+            console.log(response);
+            var data = response.data;
+            if (data.code == 0)
+                successCallback(data.data);
+            else
+                errorCallback(data.code);
+        }, function (response) {
+            console.log(response);
+            errorCallback(-1);
+        });
     };
     return service;
 }])
