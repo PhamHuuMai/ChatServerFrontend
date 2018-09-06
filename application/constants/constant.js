59ac04bba1daef2aab9b953e19b49067922f3d73
DESKTOP-FJ00PFP\MaiPH <mai.pham.intern@ntq-solution.com.vn>
2018-09-07 00:26:56 +0700

    cc

--- application/constants/constant.js
+++ application/constants/constant.js
@@ -1,5 +1,7 @@
-var apiHost = "https://chatserver-maiph.herokuapp.com";
+// var apiHost = "https://chatserver-maiph.herokuapp.com";
 // var apiHost = "http://localhost:1997";
-var socketChatHost = "ws://chatserver-maiph.herokuapp.com/chat";
+var apiHost = "http://103.81.85.169:1997";
+// var socketChatHost = "ws://chatserver-maiph.herokuapp.com/chat";
 // var socketChatHost = "ws://localhost:1997/chat";
+var socketChatHost = "ws://103.81.85.169:1997/chat";
 var ws;
